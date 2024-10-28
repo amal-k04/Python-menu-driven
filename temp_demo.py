131a4d94d76bf59397d8093df2d291ded44012a6
AMAL K PHILIP <amal17philip@gmail.com>
2024-10-28 10:29:50 +0530

    Add files via upload

---
+++ temp_demo.py
@@ -0,0 +1,23 @@
+'''
+Author:Amal K Philip
+Date:28-10-2024
+Description:program that simulates a simple bank ATM system
+'''
+while True:
+    print("\t1.Celsius to Fahrenheit")
+    print("\t2.Fahrenheit to Celsius")
+    print("\t3.Exit")
+    choice=int(input("Enter your choice:"))
+    if choice==3:
+        print("BYE BYEE")
+        break
+    elif choice==1:
+        celsius=float(input("Enter the Celsius:"))
+        fahrenheit=(celsius * 9/5) + 32
+        print(f"Temperature in Fahrenheit is :{fahrenheit}")
+    elif choice==2:
+        fahrenheit=float(input("Enter the Fahrenheit:"))
+        celsius=(fahrenheit - 32) * 5/9
+        print(f"Temperature in Celsius is :{celsius}")
+    else:
+        print("INVALID ERROR")
